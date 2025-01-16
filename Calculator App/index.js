c7549d8b013bc6625f462a89e0a2d4afae4d1813
brykdawid <brykdawid1@gmail.com>
2025-01-16 19:36:00 +0100

    deleteLast()

--- Calculator App/index.js
+++ Calculator App/index.js
@@ -7,7 +7,10 @@ function appendToDisplay(input){
 function clearDisplay(){
     display.value = "";
 }
+function deleteLast(){
 
+    display.value = display.value.slice(0,-1)
+}
 
 function calculate(){
     try{
