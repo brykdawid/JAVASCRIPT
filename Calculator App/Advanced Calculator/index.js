39468a6196e5ed84f6f6dabe969c4053761ee144
brykdawid <brykdawid1@gmail.com>
2025-01-19 00:06:25 +0100

    advanced cal etc

--- Calculator App/Advanced Calculator/index.js
+++ Calculator App/Advanced Calculator/index.js
@@ -10,15 +10,18 @@ function clearAll(){
 
 function calculate(){
     try{
-        if(display.value.includes("^")){
+        if(display.value.includes("^") && display.value.includes("e")){
+            display.value = display.value.replaceAll("^", "**")
+            display.value = display.value.replaceAll("e","2.71")
+        }
+        else if(display.value.includes("^" )){
             display.value = display.value.replaceAll("^", "**")
             display.value = eval(display.value)
         }
         else if(display.value.includes("e")){
-            display.value = display.value.replaceAll("e","2,71")
-            display.value = eval(display.value)
+            display.value = display.value.replaceAll("e",2,71)
+            display.value = eval(display.value.toFixed(2))
         }
-
         else{
         display.value = eval(display.value)
         }
@@ -42,12 +45,54 @@ function pow3rd(input){
 }
 function powE(){
     const values = display.value.split(/[-,+/%]/);
-    
-    // Pobierz ostatnią wartość z tablicy
     const lastValue = values.at(-1);
-
-    // Utwórz nową wartość w formacie e^lastValue
     display.value = `e^${lastValue}`;
 
+}
+function pow10(){
+    const values = display.value.split(/[-,+/%]/);
+    const lastValue = values.at(-1);
+    display.value = `10^${lastValue}`;
+}
+function percentage(input){
+    display.value += input;
+    const values = display.value.split(/[-,+/%]/);
+    console.log(values)
+    const firstValue = values.at(0)
+    console.log(firstValue)
+    const lastValue = values.at(-2)
+    console.log(lastValue)
+    
+    const percentageNum = firstValue*lastValue/100
+    console.log(percentageNum)
+    display.value = firstValue-percentageNum
 
+}
+
+function factorial(input){
+    display.value += input
+    const value = display.value.split(/[!]/)
+    const number = value.at(0)
+    console.log(number)
+    
+
+    let numbers = [];
+    for(let i = number; i>0; i--){
+        numbers.push(i);
+    }
+    console.log(numbers)
+    function totalNum(accelerator, number){
+        return accelerator*number
+    }
+    
+    const total = numbers.reduce(totalNum)
+
+    display.value = total
+    
+}
+
+
+function hdmi(){
+    const hdmi = `podepnij tutaj`
+    console.log(hdmi)
 }
